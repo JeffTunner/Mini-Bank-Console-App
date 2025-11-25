18a18cc954963e5e6c06596512c0d1ee28e71d3c
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-25 12:30:32 +0530

    Added custom exceptions

---
+++ src/main/java/org/example/exception/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+public class InsufficientFundsException extends RuntimeException {
+
+    public static void InsufficientFundsException() {
+        System.out.println("Insufficient Funds!!!");
+    }
+}
